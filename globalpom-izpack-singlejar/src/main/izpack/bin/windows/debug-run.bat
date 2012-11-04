71b6bf2e75e3e85603a154a67e0d320116ef9184
Erwin Mueller <erwin.mueller@deventm.org>
2012-11-04 10:04:21 +0100

    Add license headers

--- globalpom-izpack-singlejar/src/main/izpack/bin/windows/debug-run.bat
+++ globalpom-izpack-singlejar/src/main/izpack/bin/windows/debug-run.bat
@@ -1,3 +1,22 @@
+@REM
+@REM Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
+@REM
+@REM This file is part of globalpom-izpack-singlejar.
+@REM
+@REM globalpom-izpack-singlejar is free software: you can redistribute it and/or modify it
+@REM under the terms of the GNU Lesser General Public License as published by the
+@REM Free Software Foundation, either version 3 of the License, or (at your
+@REM option) any later version.
+@REM
+@REM globalpom-izpack-singlejar is distributed in the hope that it will be useful, but
+@REM WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+@REM FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+@REM details.
+@REM
+@REM You should have received a copy of the GNU Lesser General Public License
+@REM along with globalpom-izpack-singlejar. If not, see <http://www.gnu.org/licenses/>.
+@REM
+
 set mainjar=%CD%\lib\${project.artifactId}-${project.version}-jar-with-dependencies.jar
 set log="-Dlog4j.configuration=file:///%CD%/etc/debug-log4j.properties"
 
