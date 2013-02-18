92ad5acc855cd8621d28d0fd7347260652f59f7c
Erwin Mueller <erwin.mueller@deventm.org>
2013-02-18 13:42:24 +0100

    Rework izpack installation

--- globalpom-izpack/src/main/izpack/bin/linux/debug-run.sh
+++ globalpom-izpack/src/main/izpack/bin/linux/debug-run.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
+#
+# Copyright 2011-2012 Erwin Müller <erwin.mueller@deventm.org>
+#
+# This file is part of globalpom-izpack.
+#
+# globalpom-izpack is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# globalpom-izpack is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with globalpom-izpack. If not, see <http://www.gnu.org/licenses/>.
+#
+
 
 symlink=`find "$0" -printf "%l"`
 cd "`dirname "${symlink:-$0}"`"
--- globalpom-izpack/src/main/izpack/bin/windows/debug-run.bat
+++ globalpom-izpack/src/main/izpack/bin/windows/debug-run.bat
@@ -1,3 +1,22 @@
+@REM
+@REM Copyright 2011-2012 Erwin Müller <erwin.mueller@deventm.org>
+@REM
+@REM This file is part of globalpom-izpack.
+@REM
+@REM globalpom-izpack is free software: you can redistribute it and/or modify it
+@REM under the terms of the GNU Lesser General Public License as published by the
+@REM Free Software Foundation, either version 3 of the License, or (at your
+@REM option) any later version.
+@REM
+@REM globalpom-izpack is distributed in the hope that it will be useful, but
+@REM WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+@REM FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+@REM details.
+@REM
+@REM You should have received a copy of the GNU Lesser General Public License
+@REM along with globalpom-izpack. If not, see <http://www.gnu.org/licenses/>.
+@REM
+
 set libs=%CD%\lib
 set mainclass="${project.custom.mainclass}"
 set log="-Dlog4j.configuration=file:///%CD%/etc/debug-log4j.properties"
