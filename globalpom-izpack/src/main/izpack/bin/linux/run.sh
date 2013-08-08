fd792e3ca8ccca1628c411db3d07e5d48f12867f
Erwin Mueller <erwin.mueller@deventm.org>
2013-08-08 17:11:54 +0200

    Replace log4j with logback configuration
    #2604

--- globalpom-izpack/src/main/izpack/bin/linux/run.sh
+++ globalpom-izpack/src/main/izpack/bin/linux/run.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 #
-# Copyright 2011-2012 Erwin Müller <erwin.mueller@deventm.org>
+# Copyright 2011-2013 Erwin Müller <erwin.mueller@deventm.org>
 #
-# This file is part of globalpom-izpack.
+# This file is part of globalpom-izpack-singlejar.
 #
-# globalpom-izpack is free software: you can redistribute it and/or modify it
+# globalpom-izpack-singlejar is free software: you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published by the
 # Free Software Foundation, either version 3 of the License, or (at your
 # option) any later version.
 #
-# globalpom-izpack is distributed in the hope that it will be useful, but
+# globalpom-izpack-singlejar is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
 # details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with globalpom-izpack. If not, see <http://www.gnu.org/licenses/>.
+# along with globalpom-izpack-singlejar. If not, see <http://www.gnu.org/licenses/>.
 #
 
 
@@ -23,8 +23,7 @@ symlink=`find "$0" -printf "%l"`
 cd "`dirname "${symlink:-$0}"`"
 
 export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on"
-libs=../../lib
-mainclass="${project.custom.mainclass}"
-log="-Dlog4j.configuration=file:///$PWD/../../etc/log4j.properties"
+mainjar="../../lib/${project.custom.jarfile}"
+log="-Dlogback.configurationFile=file:///$PWD/../../etc/logback.xml"
 
-java "$log" -cp "$libs/*" "$mainclass" $*
+java "$log" -jar "$mainjar" $*
--- globalpom-izpack/src/main/izpack/bin/windows/debug-run.bat
+++ globalpom-izpack/src/main/izpack/bin/windows/debug-run.bat
@@ -1,24 +1,23 @@
 @REM
-@REM Copyright 2011-2012 Erwin Müller <erwin.mueller@deventm.org>
+@REM Copyright 2011-2013 Erwin Müller <erwin.mueller@deventm.org>
 @REM
-@REM This file is part of globalpom-izpack.
+@REM This file is part of globalpom-izpack-singlejar.
 @REM
-@REM globalpom-izpack is free software: you can redistribute it and/or modify it
+@REM globalpom-izpack-singlejar is free software: you can redistribute it and/or modify it
 @REM under the terms of the GNU Lesser General Public License as published by the
 @REM Free Software Foundation, either version 3 of the License, or (at your
 @REM option) any later version.
 @REM
-@REM globalpom-izpack is distributed in the hope that it will be useful, but
+@REM globalpom-izpack-singlejar is distributed in the hope that it will be useful, but
 @REM WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 @REM FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
 @REM details.
 @REM
 @REM You should have received a copy of the GNU Lesser General Public License
-@REM along with globalpom-izpack. If not, see <http://www.gnu.org/licenses/>.
+@REM along with globalpom-izpack-singlejar. If not, see <http://www.gnu.org/licenses/>.
 @REM
 
-set libs=%CD%\lib
-set mainclass="${project.custom.mainclass}"
-set log="-Dlog4j.configuration=file:///%CD%/etc/debug-log4j.properties"
+set mainjar="%CD%"\lib\\${project.custom.jarfile}
+set log="-Dlogback.configurationFile=file:///%CD%/etc/logback-debug.xml"
 
-java %log% -cp "%libs%/*"; %mainclass%
+java %log% -jar %mainjar%
